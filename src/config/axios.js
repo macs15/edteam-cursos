caa8e848d6705a4f2053920ba589b0469aafb797
maykell armando carrillo sotillo <maykellcarrillo@gmail.com>
2020-11-21 22:15:59 -0300

    usando url estatica para evitar conflictos en el test

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: process.env.REACT_APP_BASE_URL
+    // baseURL: process.env.REACT_APP_BASE_URL
+    baseURL: 'http://localhost:3001/'
 })
 
 export default axiosClient;
