317627b3760716c6f9606cc6254b5799125dffa3
maykell armando carrillo sotillo <maykellcarrillo@gmail.com>
2020-11-21 22:21:07 -0300

    estilos movidos para ahorrar lineas de codigo en componente

--- src/components/CursoPreview.js
+++ src/components/CursoPreview.js
@@ -1,73 +1,8 @@
-import styled from '@emotion/styled';
 import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 import axiosClient from '../config/axios';
 import CursoContext from '../context/CursoContext';
-import { ButtonsContainer } from './utils/styledComponents';
-
-const CardContainer = styled.li`
-    list-style: none;
-    width: 33%;
-    max-width: 250px;
-    min-width: 250px;
-    flex: 1;
-    background-color: #fff;
-    margin: 0 1rem 2rem 1rem;
-    display: flex;
-    flex-direction: column;
-    --tw-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1), 0 2px 4px -1px rgba(0, 0, 0, 0.06);
-    box-shadow: var(--tw-ring-offset-shadow, 0 0 #0000), var(--tw-ring-shadow, 0 0 #0000), var(--tw-shadow);
-    border-radius: 0 0 10px 10px;
-    .img-container {
-        width: 100%;
-        img {
-            border-radius: 10px 10px 0 0;
-            width: 100%;
-            background-position: center;
-            background-size: cover; 
-        }
-    }
-
-    .details-container {
-        padding: 2rem 2rem 1rem 2rem;
-        height: auto;
-        .title-container {
-            a {
-                text-decoration: none;
-                font-size: 1.4rem;
-            }
-        }
-        .description-text {
-            padding: 1rem 0;
-            font-size: 1.4rem;
-            color: #697477;
-        }
-    }
-    .tag {
-        padding: 1rem;
-        margin-top: auto;
-
-        .aviable {
-            color: green;
-        }
-        .soon {
-            color: #5e5eff;
-        }
-    }
-    .footer {
-        display: flex;
-        justify-content: space-between;
-        height: auto;
-        padding: 1rem;
-        background-color: #e0e1e1;
-    }
-
-    @media (max-width: 640px) {
-        width: 90%;
-        max-width: none;
-        min-width: auto;
-    }
-`;
+import { ButtonsContainer, CardContainer } from './utils/styledComponents';
 
 
 const CursoPreview = ({curso}) => {
@@ -102,7 +37,9 @@ const CursoPreview = ({curso}) => {
     return ( 
         <CardContainer>
             <div className="img-container">
-                <img src={imagen} alt={nombre} />
+                <a href={`/cursos/${id}`}>
+                    <img src={imagen} alt={nombre} />
+                </a>
             </div>
             <div className="details-container">
                 <div className="title-container">
