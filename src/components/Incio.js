94c4c09ac6592dce70ada103dc5176a494262e7b
maykell armando carrillo sotillo <maykellcarrillo@gmail.com>
2020-11-21 22:16:52 -0300

    estilos y contenido generales

--- src/components/Incio.js
+++ src/components/Incio.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import Navegacion from './Layout/Navegacion';
+import React from "react";
+import Navegacion from "./Layout/Navegacion";
+import { HomeContainer } from './utils/styledComponents';
 
 const Inicio = () => {
-    return ( 
-        <>
-            <Navegacion />
-            <h1 style={{marginTop: '70px'}}>Inicio</h1>
-        </>
-     );
-}
- 
+  return (
+    <>
+      <Navegacion />
+      <HomeContainer>
+        <h2>Bienvenido a EDteam Cursos</h2>
+        <div className="box-banner">
+          <div className="container-banner">
+            <div className="info-banner">
+              <h1>
+                Aprende las <span>mejores tecnologías</span> con nuestros cursos
+              </h1>
+              <p className="text">
+                Domina las mejores tecnologías en un solo lugar y conviertete en
+                un profesional capaz de lograr grandes resultados. Alístate, qué
+                esperas?
+              </p>
+              <div className="btn-container">
+                <a href="/cursos" className="btn">Ver todos los cursos</a>
+              </div>
+            </div>
+
+            <div className="img-container">
+              <img
+                src="https://edteam-media.s3.amazonaws.com/courses/big/e7fbb7f9-773c-426a-bbb5-14276a37af33.png"
+                alt="Cursos de desarrollo web"
+              />
+            </div>
+          </div>
+        </div>
+      </HomeContainer>
+    </>
+  );
+};
+
 export default Inicio;
